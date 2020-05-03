f96cbaf262343298cf42511760dd6f1a38c98a8c
leandroprada <leandroprada@gmail.com>
2020-05-02 21:16:16 -0300

    adding login for special area

--- login/signup.php
+++ login/signup.php
@@ -6,14 +6,6 @@ require 'dbconn.inc.php';
  $username = $_POST['username'];
  $password = $_POST['password'];
  $sql = "SELECT username FROM users WHERE username=?";
-//this code is to get the user name
-	$query1 = "SELECT name FROM usr WHERE username=";
-	$query2 = '"'.$username.'";';
-	$query = $query1.$query2;
-	$result = mysqli_query($conn,$query);
-	$row = mysqli_fetch_row($result);
-	$_SESSION['name'] = $row[0];
-
  
  $stmt = mysqli_stmt_init($conn);
 

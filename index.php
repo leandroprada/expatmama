4f3bd8913bfc48ceb5f9ce28f90b924367a74f6c
leandroprada <leandroprada@gmail.com>
2020-05-02 21:41:54 -0300

    adding login for special area

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ $userlogged = $_SESSION['name'];
 										data-splitout="none"
 										data-responsive_offset="on"
 										style="z-index: 6; white-space: nowrap;">
-										<h1>Welcome <?php echo .$userlogged. ?> to Expat MAMA!</h1>
+										<h1>Welcome <?php echo $userlogged; ?> to Expat MAMA!</h1>
 									</div>
 
 									<!-- LAYER NR. 2 -->

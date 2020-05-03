5eeadc2c4eb897c0ac7ca06b52bf9075c011b09f
leandroprada <leandroprada@gmail.com>
2020-05-02 21:57:57 -0300

    adding login for special area

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
  session_start();
 $userlogged = $_SESSION['name'];
-
+(!isset($userlogged))  {
+				header("Location: ./login/login.php");
+					}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,17 +44,16 @@ $userlogged = $_SESSION['name'];
 	<body>
 		<div class="main-page-wrapper">
 
-		<?php if (isset($userlogged))  {
-			
-			echo '
-				<!--
+
+
+			<!--
 			=============================================
 				Theme Header
 			==============================================
-			-->';
+			-->
 			
-			require "header.php";
-			echo '
+				<?php require "header.php";?>
+
 			<!--
 			=====================================================
 				Theme Main SLider
@@ -127,7 +128,7 @@ $userlogged = $_SESSION['name'];
 										data-splitin="none"
 										data-splitout="none"
 										data-responsive_offset="on">
-										<a href="./login/login.php" class="project-button hvr-bounce-to-right">View Content!</a>
+										<a href="#about-us" class="project-button hvr-bounce-to-right">View Content!</a>
 									</div>
 
 								</li>
@@ -376,108 +377,9 @@ $userlogged = $_SESSION['name'];
   ldk.src = 'https://s.cliengo.com/weboptimizer/5d319819e4b0f905135b7465/5d31981ae4b0f905135b7468.js';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(ldk, s);
-})();</script>'
+})();</script>
             
 
-;
-				
-			}
-			
-			elseif (!isset($userlogged))  { echo '
-					<!--
-			=============================================
-				Theme Header
-			==============================================
-			-->';
-			
-			require "header.php";
-			echo '
-			<!--
-			=====================================================
-				Theme Main SLider
-			=====================================================
-			-->
-			<div id="home" class="banner">
-	        	<div class="rev_slider_wrapper">
-					<!-- START REVOLUTION SLIDER 5.0.7 auto mode -->
-						<div id="main-banner-slider" class="rev_slider video-slider" data-version="5.0.7">
-							<ul>
 
-								<!-- SLIDE1  -->
-								<li data-index="rs-280" data-transition="fade" data-slotamount="default" data-easein="default" data-easeout="default" data-masterspeed="default"  data-title="Expat Mama!" data-description="">
-									<!-- MAIN IMAGE -->
-									<img src="images/home/slide-1.jpg"  alt="image" class="rev-slidebg" data-bgparallax="3" data-bgposition="center center" data-duration="20000" data-ease="Linear.easeNone" data-kenburns="on" data-no-retina="" data-offsetend="0 0" data-offsetstart="0 0" data-rotateend="0" data-rotatestart="0" data-scaleend="100" data-scalestart="140">
-									<!-- LAYERS -->
-
-									<!-- LAYER NR. 1 -->
-									<div class="tp-caption"
-										data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-										data-y="['middle','middle','middle','middle']" data-voffset="['-58','-58','0','-50']"
-										data-width="none"
-										data-height="none"
-										data-whitespace="nowrap"
-										data-transform_idle="o:1;"
-
-										data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-										data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-										data-mask_in="x:0px;y:[100%];"
-										data-mask_out="x:inherit;y:inherit;"
-										data-start="1000"
-										data-splitin="none"
-										data-splitout="none"
-										data-responsive_offset="on"
-										style="z-index: 6; white-space: nowrap;">
-										<h1>Welcome to Expat MAMA!</h1>
-									</div>
-
-									<!-- LAYER NR. 2 -->
-									<div class="tp-caption"
-										data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-										data-y="['middle','middle','middle','middle']" data-voffset="['-05','-05','63','0']"
-										data-width="none"
-										data-height="none"
-										data-whitespace="nowrap"
-										data-transform_idle="o:1;"
-
-										data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-										data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-										data-mask_in="x:0px;y:[100%];"
-										data-mask_out="x:inherit;y:inherit;"
-										data-start="2000"
-										data-splitin="none"
-										data-splitout="none"
-										data-responsive_offset="on"
-										style="z-index: 6; white-space: nowrap;">
-									</div>
-
-
-									<!-- LAYER NR. 3 -->
-									<div class="tp-caption"
-										data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-										data-y="['middle','middle','middle','middle']" data-voffset="['52','52','125','80']"
-										data-transform_idle="o:1;"
-
-										data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-										data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-										data-mask_in="x:0px;y:[100%];"
-										data-mask_out="x:inherit;y:inherit;"
-										data-start="3000"
-										data-splitin="none"
-										data-splitout="none"
-										data-responsive_offset="on">
-										<a href="#about-us" class="project-button hvr-bounce-to-right"> Please Login to View Content!</a>
-									</div>
-
-								</li>
-							</ul>
-						</div>
-					</div><!-- END REVOLUTION SLIDER -->
-	        </div> <!--  /#banner -->'
-
-
-					}
-			?>
-
-			
 	</body>
 </html>
